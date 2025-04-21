f56de888b22e3815fc3c595d06f6f8ec5b00b7aa
julien genty <mp.dev.mg@gmail.com>
2025-04-21 16:37:55 +0200

    Exercise seven

---
+++ Exercice7/index.js
@@ -0,0 +1,20 @@
+
+/** - - -
+ * 🕵️ This function converts the number contained in the 
+ * 
+ * text input into its binary representation.
+ */
+function convertToBinary() {
+    const decimalInput = document.getElementById('decimalInput').value;
+    const binaryResult = document.getElementById('binaryResult');
+
+    const decimalNumber = Number(decimalInput);
+
+    if (isNaN(decimalNumber)) {
+        binaryResult.textContent = '';
+        return;
+    };
+
+    const binary = decimalNumber.toString(2);
+    binaryResult.textContent = binary;
+};
